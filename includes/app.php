1026677dc317f77c2a6ebf6bc8ac2de3d8b67044
Alex Garcia <105954153+Alex-Gar@users.noreply.github.com>
2024-06-25 14:20:23 -0600

    Update app.php
    Cambios en la configuración

--- includes/app.php
+++ includes/app.php
@@ -1,12 +1,10 @@
 <?php 
-
+use Model\ActiveRecord;
 require 'funciones.php';
 require 'database.php';
 require __DIR__ . '/../vendor/autoload.php';
 
-// Conectarnos a la base de datos
-use Model\ActiveRecord;
-ActiveRecord::setDB($db);
-
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->safeLoad();
+// Conectarnos a la base de datos
+ActiveRecord::setDB($db);
